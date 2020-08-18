56907d6deca90e1c49077bed6ef1a3441550f921
traviscult <60018617+traviscult@users.noreply.github.com>
2020-08-18 11:25:04 -0400

    added contact info

--- src/views/Index.js
+++ src/views/Index.js
@@ -68,20 +68,23 @@ function LandingPage() {
       <ExamplesNavbar />
       <LandingPageHeader />
       <div className="main">
-        <div 
-        // style={{
-        //   backgroundImage:
-        //     "url(" + require("assets/img/watercolor.jpg") + ")",
-        // }}
-        className="section text-center">
+        <div
+          // style={{
+          //   backgroundImage:
+          //     "url(" + require("assets/img/watercolor.jpg") + ")",
+          // }}
+          className="section text-center"
+        >
           <Container>
             <Row>
               <Col className="ml-auto mr-auto" md="8">
                 <h2 className="title">About</h2>
                 <h5 className="description">
-                My name is Travis. I am California born but was raised on Long Island NY.
-							  I have a background working most aspects of the music and film industry.
-							  I have a strong passion for filmmaking, photography, and information technology.                </h5>
+                  My name is Travis. I am California born but was raised on Long
+                  Island NY. I have a background working most aspects of the
+                  music and film industry. I have a strong passion for
+                  filmmaking, photography, and information technology.{" "}
+                </h5>
                 <br />
                 {/* <Button
                   className="btn-round"
@@ -95,6 +98,16 @@ function LandingPage() {
             </Row>
             <br />
             <br />
+            <Row>
+              <Col className="ml-auto mr-auto" md="8"></Col>
+              <h2 className="title">Skills</h2>
+              <h5 className="description">
+                My name is Travis. I am California born but was raised on Long
+                Island NY. I have a background working most aspects of the music
+                and film industry. I have a strong passion for filmmaking,
+                photography, and information technology.{" "}
+              </h5>
+            </Row>
             {/* <Row>
               <Col md="3">
                 <div className="info">
@@ -178,7 +191,11 @@ function LandingPage() {
               <Col md="4">
                 <Card className="card-plain">
                   <div className="card-avatar">
-                    <a target= "_blank"  rel="noopener noreferrer" href="https://parks-and-weather.herokuapp.com/login_new.html">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://parks-and-weather.herokuapp.com/login_new.html"
+                    >
                       <img
                         alt="..."
                         src={require("assets/img/projects/NPS.png")}
@@ -186,14 +203,24 @@ function LandingPage() {
                     </a>
                   </div>
                   <CardBody>
-                    <a href="https://parks-and-weather.herokuapp.com/login_new.html" onClick={(e) => e.preventDefault()}>
+                    <a
+                      href="https://parks-and-weather.herokuapp.com/login_new.html"
+                      onClick={(e) => e.preventDefault()}
+                    >
                       <div className="author">
                         <CardTitle tag="h4">National Park Service</CardTitle>
                         <h6 className="card-category">Application</h6>
                       </div>
                     </a>
                     <p className="card-description text-center">
-                    This application allows you to create an account and search through the United States and Washington D.C. Once you have made a state selection you are presented with a list of historical locations, national parks, and interesting places to visit in that given state. You can also create blog posts about the things you have seen or done. When you login you will also get weather forecast for your current location.
+                      This application allows you to create an account and
+                      search through the United States and Washington D.C. Once
+                      you have made a state selection you are presented with a
+                      list of historical locations, national parks, and
+                      interesting places to visit in that given state. You can
+                      also create blog posts about the things you have seen or
+                      done. When you login you will also get weather forecast
+                      for your current location.
                     </p>
                   </CardBody>
                   <CardFooter className="text-center">
@@ -205,8 +232,7 @@ function LandingPage() {
                       href="https://github.com/traviscult/Project-2"
                       // onClick={(e) => e.preventDefault()}
                     >
-                      <i className="fa fa-github" 
-                      />
+                      <i className="fa fa-github" />
                     </Button>
                   </CardFooter>
                 </Card>
@@ -214,7 +240,11 @@ function LandingPage() {
               <Col md="4">
                 <Card className="card-plain">
                   <div className="card-avatar">
-                    <a target="_blank" rel="noopener noreferrer" href="https://traviscult.github.io/Date-Night/">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://traviscult.github.io/Date-Night/"
+                    >
                       <img
                         alt="Date Night Application"
                         src={require("assets/img/projects/DateNight.png")}
@@ -222,16 +252,22 @@ function LandingPage() {
                     </a>
                   </div>
                   <CardBody>
-                      <div className="author">
-                        <CardTitle tag="h4">Date Night</CardTitle>
-                        <h6 className="card-category">Application</h6>
-                      </div>
+                    <div className="author">
+                      <CardTitle tag="h4">Date Night</CardTitle>
+                      <h6 className="card-category">Application</h6>
+                    </div>
                     <p className="card-description text-center">
-                    Have you ever asked your date what they wanted to eat and they respond back with the dreaded "I don't know, what do you want to eat?" Or do you just want to surprise your date and show them are sophisticated and culturally diverse you are? Or do you want to add culturally diversity to your recipe book? This is the website for you!
+                      Have you ever asked your date what they wanted to eat and
+                      they respond back with the dreaded "I don't know, what do
+                      you want to eat?" Or do you just want to surprise your
+                      date and show them are sophisticated and culturally
+                      diverse you are? Or do you want to add culturally
+                      diversity to your recipe book? This is the website for
+                      you!
                     </p>
                   </CardBody>
                   <CardFooter className="text-center">
-                  <Button
+                    <Button
                       className="btn-just-icon btn-neutral"
                       color="link"
                       target="_blank"
@@ -239,8 +275,7 @@ function LandingPage() {
                       href="https://github.com/traviscult/Date-Night"
                       // onClick={(e) => e.preventDefault()}
                     >
-                      <i className="fa fa-github" 
-                      />
+                      <i className="fa fa-github" />
                     </Button>
                   </CardFooter>
                 </Card>
@@ -248,7 +283,11 @@ function LandingPage() {
               <Col md="4">
                 <Card className="card-plain">
                   <div className="card-avatar">
-                    <a target="_blank" rel="noopener noreferrer" href="https://5f0b929efa9b8814d9b74472--react-img-directory-app.netlify.app/">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://5f0b929efa9b8814d9b74472--react-img-directory-app.netlify.app/"
+                    >
                       <img
                         alt="..."
                         src={require("assets/img/projects/employeeDirectory.png")}
@@ -256,16 +295,21 @@ function LandingPage() {
                     </a>
                   </div>
                   <CardBody>
-                      <div className="author">
-                        <CardTitle tag="h4">Employee Directory</CardTitle>
-                        <h6 className="card-category">Application</h6>
-                      </div>
+                    <div className="author">
+                      <CardTitle tag="h4">Employee Directory</CardTitle>
+                      <h6 className="card-category">Application</h6>
+                    </div>
                     <p className="card-description text-center">
-                    A react application that allows you to access your companies employees information. An employee or manager would benefit greatly from being able to view non-sensitive data about other employees. It would be particularly helpful to be able to filter employees by name.
+                      A react application that allows you to access your
+                      companies employees information. An employee or manager
+                      would benefit greatly from being able to view
+                      non-sensitive data about other employees. It would be
+                      particularly helpful to be able to filter employees by
+                      name.
                     </p>
                   </CardBody>
                   <CardFooter className="text-center">
-                  <Button
+                    <Button
                       className="btn-just-icon btn-neutral"
                       color="link"
                       target="_blank"
@@ -273,8 +317,7 @@ function LandingPage() {
                       href="https://github.com/traviscult/employee-directory"
                       // onClick={(e) => e.preventDefault()}
                     >
-                      <i className="fa fa-github" 
-                      />
+                      <i className="fa fa-github" />
                     </Button>
                   </CardFooter>
                 </Card>
@@ -284,7 +327,11 @@ function LandingPage() {
               <Col md="4">
                 <Card className="card-plain">
                   <div className="card-avatar">
-                    <a target= "_blank"  rel="noopener noreferrer" href="https://workout-tracker-nosql-hw.herokuapp.com/">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://workout-tracker-nosql-hw.herokuapp.com/"
+                    >
                       <img
                         alt="Fitness Tracker application"
                         src={require("assets/img/projects/fitnessTracker.png")}
@@ -292,14 +339,20 @@ function LandingPage() {
                     </a>
                   </div>
                   <CardBody>
-                    <a href="https://workout-tracker-nosql-hw.herokuapp.com/" onClick={(e) => e.preventDefault()}>
+                    <a
+                      href="https://workout-tracker-nosql-hw.herokuapp.com/"
+                      onClick={(e) => e.preventDefault()}
+                    >
                       <div className="author">
                         <CardTitle tag="h4">Fitness Tracker</CardTitle>
                         <h6 className="card-category">Application</h6>
                       </div>
                     </a>
                     <p className="card-description text-center">
-                    A workout tracking application utilizing a Mongo database with a Mongoose schema and handling the routes with Express.                    </p>
+                      A workout tracking application utilizing a Mongo database
+                      with a Mongoose schema and handling the routes with
+                      Express.{" "}
+                    </p>
                   </CardBody>
                   <CardFooter className="text-center">
                     <Button
@@ -310,8 +363,7 @@ function LandingPage() {
                       href="https://github.com/traviscult/Workout-Tracker"
                       // onClick={(e) => e.preventDefault()}
                     >
-                      <i className="fa fa-github" 
-                      />
+                      <i className="fa fa-github" />
                     </Button>
                   </CardFooter>
                 </Card>
@@ -319,7 +371,11 @@ function LandingPage() {
               <Col md="4">
                 <Card className="card-plain">
                   <div className="card-avatar">
-                    <a target="_blank" rel="noopener noreferrer" href="https://whispering-earth-86802.herokuapp.com">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://whispering-earth-86802.herokuapp.com"
+                    >
                       <img
                         alt="Date Night Application"
                         src={require("assets/img/projects/burger.png")}
@@ -327,15 +383,18 @@ function LandingPage() {
                     </a>
                   </div>
                   <CardBody>
-                      <div className="author">
-                        <CardTitle tag="h4">Burger Tracker</CardTitle>
-                        <h6 className="card-category">Application</h6>
-                      </div>
+                    <div className="author">
+                      <CardTitle tag="h4">Burger Tracker</CardTitle>
+                      <h6 className="card-category">Application</h6>
+                    </div>
                     <p className="card-description text-center">
-                    A burger logger utilizing MySQL, Node, Express, Handlebars and a homemade ORM. The final application was deployed through Heroku.                    </p>
+                      A burger logger utilizing MySQL, Node, Express, Handlebars
+                      and a homemade ORM. The final application was deployed
+                      through Heroku.{" "}
+                    </p>
                   </CardBody>
                   <CardFooter className="text-center">
-                  <Button
+                    <Button
                       className="btn-just-icon btn-neutral"
                       color="link"
                       target="_blank"
@@ -343,8 +402,7 @@ function LandingPage() {
                       href="https://github.com/traviscult/Node-Express-Handlebars"
                       // onClick={(e) => e.preventDefault()}
                     >
-                      <i className="fa fa-github" 
-                      />
+                      <i className="fa fa-github" />
                     </Button>
                   </CardFooter>
                 </Card>
@@ -352,7 +410,11 @@ function LandingPage() {
               <Col md="4">
                 <Card className="card-plain">
                   <div className="card-avatar">
-                    <a target="_blank" rel="noopener noreferrer" href="https://notetaker050120.herokuapp.com/">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://notetaker050120.herokuapp.com/"
+                    >
                       <img
                         alt="Note taker application"
                         src={require("assets/img/projects/notes.png")}
@@ -360,15 +422,18 @@ function LandingPage() {
                     </a>
                   </div>
                   <CardBody>
-                      <div className="author">
-                        <CardTitle tag="h4">Notes</CardTitle>
-                        <h6 className="card-category">Application</h6>
-                      </div>
+                    <div className="author">
+                      <CardTitle tag="h4">Notes</CardTitle>
+                      <h6 className="card-category">Application</h6>
+                    </div>
                     <p className="card-description text-center">
-                    An application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.                    </p>
+                      An application that can be used to write, save, and delete
+                      notes. This application will use an express backend and
+                      save and retrieve note data from a JSON file.{" "}
+                    </p>
                   </CardBody>
                   <CardFooter className="text-center">
-                  <Button
+                    <Button
                       className="btn-just-icon btn-neutral"
                       color="link"
                       target="_blank"
@@ -376,8 +441,7 @@ function LandingPage() {
                       href="https://github.com/traviscult/Notes"
                       // onClick={(e) => e.preventDefault()}
                     >
-                      <i className="fa fa-github" 
-                      />
+                      <i className="fa fa-github" />
                     </Button>
                   </CardFooter>
                 </Card>
@@ -389,22 +453,26 @@ function LandingPage() {
           <Container>
             <Row>
               <Col className="ml-auto mr-auto" md="8">
-                <h2 className="text-center">Get in touch?</h2>
-                <Form className="contact-form" >
+                <h2 className="text-center">Get in touch</h2>
+                <Form className="text-center" className="contact-form">
                   <Row>
-                    <Col md="6">
-                      <label>Name</label>
+                    <Col className="text-center">
+                      <h5>Name</h5>
                       <p>Travis Cultreri</p>
                     </Col>
-                    </Row>
-                    <Row>
-                    <Col md="6">
-                      <label>Email</label>
+                  </Row>
+                  <Row>
+                    <Col className="text-center">
+                      <h5>Email</h5>
                       <p>traviscult@gmail.com</p>
                     </Col>
                   </Row>
-                  <label>Phone Number</label>
-                  <p>310.339.7695</p>
+                  <Row>
+                    <Col className="text-center">
+                      <h5>Phone Number</h5>
+                      <p>310.339.7695</p>
+                    </Col>
+                  </Row>
                 </Form>
               </Col>
             </Row>
